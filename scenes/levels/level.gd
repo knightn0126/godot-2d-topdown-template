664e41bdf91caa7db993b75f17414725c88ec089
Stefano Mercadante <stefmercadante@gmail.com>
2025-02-05 16:49:56 +0100

    add button to clear tilemap layers in Level

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,5 +1,10 @@
+@tool
 extends Node2D
 class_name Level
+
+@export_tool_button("Clear Tilemap Layers", "Callable") var clear_action = clear_tilemap_layers
+
+@onready var tilemap_layers: Node2D = %Layers
 
 var destination_name: String ## Used when moving between levels to get the right destination position for the player in the loaded level.
 var player_id: int ## Used when moving between levels to save the player facing direction.
@@ -22,3 +27,8 @@ func receive_data(data):
 		destination_name = data.destination_name
 	if data.player_id:
 		player_id = data.player_id
+
+func clear_tilemap_layers():
+	for node in tilemap_layers.get_children():
+		if node is TileMapLayer:
+			node.clear()
