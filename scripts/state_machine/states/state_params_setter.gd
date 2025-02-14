d7ac9d278ae705afd76f993f175af9d2526afd48
Stefano Mercadante <stefmercadante@gmail.com>
2025-02-14 16:23:07 +0100

    fix StateParamsSetter bug

--- scripts/state_machine/states/state_params_setter.gd
+++ scripts/state_machine/states/state_params_setter.gd
@@ -1,19 +1,28 @@
-@tool
-extends State
-## Set some properties to a node present in the parent StateMachine params.
+## A state that automatically sets properties on a target node when entering or exiting.  
+## Properties can be set directly on a specified node or through a parameter key in the StateMachine.
 class_name StateParamsSetter
+extends State
 
 @export_category("Set Properties")
-@export var node: Node: ## The node in which you want to set the properties.[br]It has priority over param_key.
+
+## The node on which properties will be set.  
+## If assigned, this takes priority over `param_key`.
+@export var node: Node:
 	set(value):
 		node = value
-		notify_property_list_changed()
-@export var param_key: String: ## Alternatively to selecting a node, you can indicate an element present in the State Machine params in which to set the properties.
+
+## The key to retrieve a target node from the StateMachine's parameters.  
+@export var param_key: String:
 	set(value):
 		param_key = value
-		notify_property_list_changed()
-@export var on_enter: Dictionary[String, Variant] ## Set some properties to the node when entering the state.
-@export var on_exit: Dictionary[String, Variant] ## Set some properties to the node when exiting the state.
+
+## Properties to set on the target node when entering the state.  
+## The dictionary keys represent property names, and the values are the values to assign.
+@export var on_enter: Dictionary[String, Variant]
+
+## Properties to set on the target node when exiting the state.  
+## The dictionary keys represent property names, and the values are the values to assign.
+@export var on_exit: Dictionary[String, Variant]
 
 func enter():
 	var set_params_on = _get_node()
@@ -30,6 +39,8 @@ func exit():
 	for prop in on_exit:
 		set_params_on.set.call_deferred(prop, on_exit[prop])
 
+## Retrieves the node to apply property changes to.  
+## Returns `node` if set, otherwise attempts to find it using `param_key` in the StateMachine.  
 func _get_node():
 	var set_params_on = null
 	if node:
@@ -37,13 +48,5 @@ func _get_node():
 	elif state_machine.params.has(param_key):
 		set_params_on = state_machine.params[param_key]
 	else:
-		push_warning("Node %s not found in %s" % [param_key, get_path()])
+		push_warning("Node '%s' not found in %s" % [param_key, get_path()])
 	return set_params_on
-
-func _validate_property(property: Dictionary) -> void:
-	if property.name == "node":
-		if param_key:
-			property.usage = PROPERTY_USAGE_NONE
-	if property.name == "param_key":
-		if node:
-			property.usage = PROPERTY_USAGE_NONE
