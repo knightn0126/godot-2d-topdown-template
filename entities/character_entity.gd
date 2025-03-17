04f99e99ac6e533067a825f1e0e13309f073d3b1
Stefano Mercadante <stefmercadante@gmail.com>
2025-03-17 17:48:48 +0100

    fix _set_weapon func

--- entities/character_entity.gd
+++ entities/character_entity.gd
@@ -108,7 +108,7 @@ func _set_weapon(_weapon: DataWeapon):
 		print_debug("%s equipped weapon: %s" % [name, weapon.resource_name])
 		if attack_cooldown_timer:
 			attack_cooldown_timer.stop()
-		hit_box.hp_change = weapon.power
+		hit_box.hp_change = -weapon.power
 
 func _init_attack_cooldown_timer():
 	attack_cooldown_timer = Timer.new()
